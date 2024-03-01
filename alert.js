0ecbf6fd352d13c2b80ebca20dbccfa869fbc8a5
cmpdchtr <151186625+cmpdchtr@users.noreply.github.com>
2024-03-01 19:05:05 +0200

    1

--- alert.js
+++ alert.js
@@ -1,13 +1,17 @@
-// Створення звукового повідомлення
-const msg = new SpeechSynthesisUtterance('Час прозвучав!');
-
-// Встановлення часу для відтворення звуку о 8:30
-const now = new Date();
-const millisTill830 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8, 30, 0, 0) - now;
-print(millisTill830);
-if (millisTill830 < 0) {
-    millisTill830 += 86400000; // якщо час вже минув, встановлюємо наступний день
-}
-setTimeout(function(){
-    window.speechSynthesis.speak(msg); // Відтворення звукового повідомлення
-}, millisTill830);
+function playSound() {
+    const audio = new Audio("music.mp3"); // Замініть на шлях до вашого звукового файлу
+    audio.play();
+  }
+  
+  function checkTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+  
+    if (hours === 19 && minutes === 6) {
+      playSound();
+    }
+  }
+  
+  setInterval(checkTime, 1000); // Перевіряти час щосекунди
+  
