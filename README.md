a9981f25311deca0d2fe8459c9045ee8c7b1c0be
cmpdchtr <151186625+cmpdchtr@users.noreply.github.com>
2024-03-03 14:14:36 +0200

    1.0

---
+++ README.md
@@ -0,0 +1,1 @@
+# monomax™ alert [🔔]
--- alert.js
+++ alert.js
@@ -1,7 +1,7 @@
 // const lesson1 = "hours === 8 && minutes === 30 && seconds === 1";
 
 function playSound() {
-    const audio = new Audio("music.mp3"); // Замініть на шлях до вашого звукового файлу
+    const audio = new Audio("oldYellowBricks.mp3"); // Замініть на шлях до вашого звукового файлу
     audio.play();
   }
   
@@ -11,8 +11,10 @@ function playSound() {
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
     console.log(hours + " " + minutes + " " + seconds);
+
+    document.getElementById("time").textContent = hours + ":" + minutes + ":" + seconds;
   
-    if ((hours === 8 && minutes === 30 && seconds === 1) || (hours === 9 && minutes === 30 && seconds === 1) || (hours === 10 && minutes === 25 && seconds === 1) || (hours === 11 && minutes === 20 && seconds === 1) || (hours === 12 && minutes === 35 && seconds === 1) || (hours === 13 && minutes === 30 && seconds === 1) || (hours === 14 && minutes === 45 && seconds === 1) || (hours === 15 && minutes === 40 && seconds === 1) || (hours === 16 && minutes === 30 && seconds === 1) || (hours === 17 && minutes === 20 && seconds === 1)) {
+    if ((hours === 14 && minutes === 14 && seconds === 1) || (hours === 9 && minutes === 30 && seconds === 1) || (hours === 10 && minutes === 25 && seconds === 1) || (hours === 11 && minutes === 20 && seconds === 1) || (hours === 12 && minutes === 35 && seconds === 1) || (hours === 13 && minutes === 30 && seconds === 1) || (hours === 14 && minutes === 45 && seconds === 1) || (hours === 15 && minutes === 40 && seconds === 1) || (hours === 16 && minutes === 30 && seconds === 1) || (hours === 17 && minutes === 20 && seconds === 1)) {
       playSound();
     }
   }
